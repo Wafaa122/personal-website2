c2643d2dd08201001f884062af380fb6547181f8
Wafaa122 <wafa_7_@hotmail.com>
2018-02-07 17:36:46 +0200

    Add files via upload
    create website

---
+++ web personal/js/work.js
@@ -0,0 +1,42 @@
+var works = [
+  {
+    title: "Coming Soon",
+    pic: "img/02.jpg",
+    url: "#index"
+  },
+  {
+    title: "Coming Soon",
+    pic: "img/03.jpg",
+    url: "#index"
+  },
+  {
+    title: "Coming Soon",
+    pic: "img/04.jpg",
+    url: "#index"
+  },
+  {
+    title: "Coming Soon",
+    pic: "img/05.jpg",
+    url: "#index"
+  },
+  {
+    title: "Coming Soon",
+    pic: "img/02.jpg",
+    url: "#index"
+  },
+  {
+    title: "Coming Soon",
+    pic: "img/03.jpg",
+    url: "#index"
+  },
+  {
+    title: "Coming Soon",
+    pic: "img/04.jpg",
+    url: "#index"
+  },
+  {
+    title: "Coming Soon",
+    pic: "img/05.jpg",
+    url: "#index"
+  }
+];
